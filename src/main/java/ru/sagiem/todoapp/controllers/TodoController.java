ea88c6b17f3cb903dd984fb51dbfaeb52aff6e65
Maksim Kashin <sagiems@gmail.com>
2025-09-25 17:20:46 +0600

    th

--- src/main/java/ru/sagiem/todoapp/controllers/TodoController.java
+++ src/main/java/ru/sagiem/todoapp/controllers/TodoController.java
@@ -1,13 +1,15 @@
 package ru.sagiem.todoapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TodoController {
 
     @GetMapping
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("data","Hello");
         return "index";
     }
 
