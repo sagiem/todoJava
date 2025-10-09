980f458d6c667736505043189f6ce1f09f48d39e
Maksim Kashin <sagiems@gmail.com>
2025-10-09 16:33:46 +0600

    set get

--- src/main/java/ru/sagiem/todoapp/model/TodoItem.java
+++ src/main/java/ru/sagiem/todoapp/model/TodoItem.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class TodoItem {
 
     public TodoItem() {
@@ -21,19 +25,4 @@ public class TodoItem {
     private Long id;
     private String title;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
