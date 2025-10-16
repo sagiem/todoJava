c6b0aad085a2f0358e7956524ea5f2e81eca2153
Maksim Kashin <sagiems@gmail.com>
2025-10-16 12:59:28 +0600

    Форма добавления задачи

--- src/main/java/ru/sagiem/todoapp/controllers/TodoController.java
+++ src/main/java/ru/sagiem/todoapp/controllers/TodoController.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.sagiem.todoapp.model.TodoItem;
 import ru.sagiem.todoapp.repositories.TodoItemRepository;
 
@@ -22,7 +24,14 @@ public class TodoController implements CommandLineRunner {
     public String index(Model model) {
         List<TodoItem> allTodos = todoItemRepository.findAll();
         model.addAttribute("allTodos", allTodos);
+        model.addAttribute("newTodo", new TodoItem());
         return "index";
+    }
+
+    @PostMapping("/add")
+    public String add(@ModelAttribute TodoItem todoItem) {
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
     }
 
     @Override
